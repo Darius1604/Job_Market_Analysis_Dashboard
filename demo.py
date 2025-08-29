b85feda7f37365425c25a4ba0df660433c608ab0
Darius <dariushileaga96@outlook.com>
2025-08-29 16:48:29 +0300

    Plot some analytics based on the scraped data using matplotlib
    Scrape only if the infos for the keyword have not
    been already scraped by checking if the
    corresponding file exists

--- demo.py
+++ demo.py
@@ -2,8 +2,14 @@ from bs4 import BeautifulSoup
 import requests
 from datetime import datetime
 import pandas as pd
+import numpy as np
 import argparse
+import os
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import matplotlib.cm as cm  # matplotlib colormap
+import matplotlib.gridspec as gridspec
+from collections import Counter
 
 
 def get_search_url(keyword):
@@ -11,13 +17,13 @@ def get_search_url(keyword):
     return f"https://m.timesjobs.com/mobile/jobs-search-result.html?txtKeywords={keyword}&cboWorkExp1=-1&txtLocation="
 
 
-def fetch_page(url): # Need to use Selenium here
+def fetch_page(url):
     response = requests.get(url)
     response.raise_for_status()
     return BeautifulSoup(response.text, 'lxml')
 
 
-def parse_job_links(soup, limit=5):
+def parse_job_links(soup, limit=25):
     # Extract the links to the job pages from the main page
     listings = soup.find_all('div', class_='srp-listing', limit=limit)
     if limit == -1:
@@ -51,8 +57,7 @@ def parse_job_details(job_url):
     location_text = location_exp_infos.find(
         'div', class_='srp-loc jd-loc').text.strip()
     location_list = location_text.split()
-    location = location_list[1].translate(str.maketrans(
-        '', '', '()/,')) + ' - ' + location_list[2].translate(str.maketrans('', '', '()/,'))
+    location = location_list[1].translate(str.maketrans('', '', '()/,'))
     years_of_experience = location_exp_infos.find(
         'div', class_='srp-exp').text.split()
     years_of_experience = years_of_experience[0] + ' ' + years_of_experience[1]
@@ -72,7 +77,7 @@ def parse_job_details(job_url):
     }
 
 
-def find_jobs(keyword, limit):
+def find_jobs(keyword, limit=25):
     base_url = get_search_url(keyword=keyword)
     soup = fetch_page(base_url)
     job_links = parse_job_links(soup, limit)
@@ -99,6 +104,77 @@ def find_jobs(keyword, limit):
         return None
 
 
+def plot_data(df):
+    if jobs_df is not None:
+        # Creates both the figure and the axes
+        fig = plt.figure(figsize=(18,10))
+        gs = gridspec.GridSpec(3,1,height_ratios=[1,1,2]) # ax3 is 2x taller
+        
+        ax1 = fig.add_subplot(gs[0])
+        ax2 = fig.add_subplot(gs[1])
+        ax3 = fig.add_subplot(gs[2])
+        ax1.set_title("Jobs per location")
+        ax2.set_title("Most popular required skills")
+        ax3.set_title("Jobs per company")
+        job_locations = df['Location'].value_counts()
+        
+        cmap = plt.get_cmap('tab20')  # pallete with max distinct colors
+        colors = cmap(np.linspace(0, 1, len(job_locations.values)))
+
+        ax1.yaxis.set_major_locator(ticker.MaxNLocator(integer=True))
+        ax1.bar(job_locations.index, job_locations.values, color=colors)
+
+        ax1.set_ylabel(f'{args.keyword.capitalize()} jobs')
+        ax1.set_title(f'{args.keyword.capitalize()} jobs by location and number')
+        
+        
+        all_skills = df['Skills'].dropna().str.split(', ') # dropna() ignores empty cells
+        
+        # Some skills are duplicated for the same job so we'll remove them using sets
+        
+        flattened_skills = []
+        
+        for sublist in all_skills:
+            clean_skills = {skill.strip().lower() for skill in sublist}
+            flattened_skills.extend(clean_skills)
+        
+        
+        skill_counts = Counter(flattened_skills)
+        skill_series = pd.Series(skill_counts).sort_values(ascending=False)
+        
+        cmap = plt.get_cmap('winter')
+        colors = cmap(np.linspace(0,1,len(skill_series.head(10))))
+        
+        
+        
+        ax2.yaxis.set_major_locator(ticker.MaxNLocator(integer=True))
+        ax2.bar(skill_series.index[:10], skill_series.values[:10],color=colors)
+        
+        ax2.set_ylabel('Number of jobs')
+        ax2.set_title('Skills')
+        
+       
+        company_names = df['Company'].value_counts()
+        # Series where index -> location names
+        #              Values -> number of jobs in each location
+        cmap = plt.get_cmap('gnuplot2')  # pallete with max distinct colors
+        colors = cmap(np.linspace(0, 1, len(company_names.values)))
+        
+        
+
+        ax3.xaxis.set_major_locator(ticker.MaxNLocator(integer=True))
+        ax3.barh(company_names.index[:10], company_names.values[:10], color=colors)
+        
+
+        ax3.set_ylabel(f'Companies')
+        ax3.set_title(f'Jobs per company')
+        ax3.set_yticks(range(len(company_names.index[:10])))
+        ax3.set_yticklabels(company_names.index[:10])
+        
+        
+        plt.show()
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()  # Create the parser instance
 
@@ -106,21 +182,15 @@ if __name__ == '__main__':
     # help = '...' shows up when we run python script.py --help
     parser.add_argument('--keyword', type=str,
                         default='python', help='Job keyword to search')
-    parser.add_argument('--limit', type=int, default=5,
+    parser.add_argument('--limit', type=int, default=25,
                         help='Number of job listings to fetch')
-    parser.add_argument('--scrape', type=bool, default= False,
+    parser.add_argument('--scrape', type=bool, default=False,
                         help='Scrape new jobs from timesjobs if scrape is True or use the ones already scraped if scrape is False')
     args = parser.parse_args()
-    if args.scrape == True:
+    if args.scrape == True or not os.path.exists(f'jobs_{datetime.today().strftime('%Y-%m-%d')}_{args.keyword}.csv'):
         jobs_df = find_jobs(args.keyword, args.limit)
     else:
         jobs_df = pd.read_csv(
             f'jobs_{datetime.today().strftime('%Y-%m-%d')}_{args.keyword}.csv')
     if jobs_df is not None:
-        location_counts = jobs_df['Location'].value_counts()
-
-        plt.figure(figsize=(8, 8))
-        plt.pie(location_counts, labels=location_counts.index,
-                autopct='%1.1f%%', startangle=140)
-        plt.title(f"Job Distribution by Location")
-        plt.show()
+        plot_data(df=jobs_df)
